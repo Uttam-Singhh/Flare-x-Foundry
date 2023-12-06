5fc2768f5c7d830587b1565350e7e517af584073
Uttam Singh <uttam@flare.network>
2023-12-06 11:03:59 +0530

    Update FundMe.t.sol

--- test/FundMe.t.sol
+++ test/FundMe.t.sol
@@ -6,41 +6,38 @@ import {FundMe} from "../src/FundMe.sol";
 import {MockFtsoRegistry} from "./mocks/MockFtsoRegistry.sol";
 
 contract CounterTest is Test {
-
     MockFtsoRegistry mockRegistry;
     FundMe public fundMe;
     address user = makeAddr("user");
     uint256 constant TEST_VALUE = 8 ether;
     uint256 constant STARTING_BALANCE = 10 ether;
-    string symbol = "testUSDC";
-    
+    string symbol = "C2FLR";
 
     function setUp() public {
         fundMe = new FundMe();
         vm.deal(user, STARTING_BALANCE);
         mockRegistry = new MockFtsoRegistry();
-        mockRegistry.setPriceForSymbol("testUSDC", 1e18, 0, 5);
+        mockRegistry.setPriceForSymbol("C2FLR", 1e18, 0, 5);
     }
 
-     //////////////////////////
-    ////// Price Tests //////
+    //////////////////////////
+    ////// Price Tests /////
     ////////////////////////
 
     function testMockPrice() public {
-        (uint256 price, uint256 timestamp, uint256 decimals) = mockRegistry.getCurrentPriceWithDecimals(symbol);
+        (uint256 price, uint256 timestamp, uint256 decimals) = mockRegistry
+            .getCurrentPriceWithDecimals(symbol);
         assertTrue(price == 1e18);
         assertTrue(decimals == 5);
         assertTrue(timestamp == 0);
     }
 
-
- //////////////////////////
-    ////// FundMe Tests //////
+    //////////////////////////
+    ////// FundMe Tests /////
     ////////////////////////
 
-
-    function testvalue() public { 
-        assertEq(fundMe.MIN_FUND(), 5e18);
+    function testvalue() public {
+        assertEq(fundMe.MIN_FUND(), 1e18);
     }
 
     function testOnlyOwnerCanWithdraw() public {
@@ -49,5 +46,4 @@ contract CounterTest is Test {
         fundMe.withdraw();
     }
 
-    // you can add more tests. for example testFundFails with less then min amount, etc.
-}
+}  //I have added only a few testcases, you can simply add more test cases like testFundFails with less then min amount, etc. the more the better!
